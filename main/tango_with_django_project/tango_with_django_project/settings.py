4082a469e20f828c7129ebf14e4dcad95ff1410b
2975774s <2975774s@student.gla.ac.uk>
2026-01-27 18:52:54 +0000

    Chapter 9

--- main/tango_with_django_project/tango_with_django_project/settings.py
+++ main/tango_with_django_project/tango_with_django_project/settings.py
@@ -20,6 +20,8 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = 'rango:login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -55,6 +57,15 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
+
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+
+
 
 TEMPLATES = [
     {
@@ -102,6 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
