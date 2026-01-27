967233147db5e1efb83e8950fb901c4611157300
2975774s <2975774s@student.gla.ac.uk>
2026-01-27 20:06:09 +0000

    Chapter 10

--- main/tango_with_django_project/tango_with_django_project/settings.py
+++ main/tango_with_django_project/tango_with_django_project/settings.py
@@ -22,6 +22,9 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'rango:login'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_AGE = 1209600
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
